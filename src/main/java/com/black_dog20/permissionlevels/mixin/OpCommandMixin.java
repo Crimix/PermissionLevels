a31f9f4a6a5a8374cceb6218f1ea86bae17e71a8
crimix <christofferm@live.dk>
2021-09-25 16:45:18 +0200

    Bumped forge to implement mixins again

--- src/main/java/com/black_dog20/permissionlevels/mixin/OpCommandMixin.java
+++ src/main/java/com/black_dog20/permissionlevels/mixin/OpCommandMixin.java
@@ -1,18 +1,18 @@
-//package com.black_dog20.permissionlevels.mixin;
-//
-//import com.mojang.brigadier.CommandDispatcher;
-//import net.minecraft.commands.CommandSourceStack;
-//import net.minecraft.server.commands.OpCommand;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//
-//@Mixin(OpCommand.class)
-//public class OpCommandMixin {
-//
-//    @Inject(method = "register(Lcom/mojang/brigadier/CommandDispatcher;)V", at = @At("HEAD"), cancellable = true)
-//    private static void register(CommandDispatcher<CommandSourceStack> source, CallbackInfo ci) {
-//        ci.cancel();
-//    }
-//} TODO When mixins are back
+package com.black_dog20.permissionlevels.mixin;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.commands.OpCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OpCommand.class)
+public class OpCommandMixin {
+
+    @Inject(method = "register(Lcom/mojang/brigadier/CommandDispatcher;)V", at = @At("HEAD"), cancellable = true)
+    private static void register(CommandDispatcher<CommandSourceStack> source, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
